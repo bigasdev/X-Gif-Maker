558f2ac85802727780df9768b605d3c747d595f0
Gustavo Domingues <61640594+bigasdev@users.noreply.github.com>
2024-06-07 08:39:14 -0300

    Removed unused curve

--- src/Scenes/GameScene.cpp
+++ src/Scenes/GameScene.cpp
@@ -5,7 +5,6 @@
 #include <cstdlib>
 #include <fstream>
 #include "../ImGui/ImSequencer.h"
-#include "../ImGui/ImCurveEdit.h"
 #include "../ImGui/imgui.h"
 #include "../ImGui/imgui_internal.h"
 
@@ -14,95 +13,6 @@ SDL_Texture* m_bg_tx_game = nullptr;
 std::vector<std::string> m_files;
 
 static const char* SequencerItemTypeNames[] = { "Camera","Music", "ScreenEffect", "FadeIn", "Animation" };
-
-struct RampEdit : public ImCurveEdit::Delegate
-{
-   RampEdit()
-   {
-      mPts[0][0] = ImVec2(-10.f, 0);
-      mPts[0][1] = ImVec2(20.f, 0.6f);
-      mPts[0][2] = ImVec2(25.f, 0.2f);
-      mPts[0][3] = ImVec2(70.f, 0.4f);
-      mPts[0][4] = ImVec2(120.f, 1.f);
-      mPointCount[0] = 5;
-
-      mPts[1][0] = ImVec2(-50.f, 0.2f);
-      mPts[1][1] = ImVec2(33.f, 0.7f);
-      mPts[1][2] = ImVec2(80.f, 0.2f);
-      mPts[1][3] = ImVec2(82.f, 0.8f);
-      mPointCount[1] = 4;
-
-
-      mPts[2][0] = ImVec2(40.f, 0);
-      mPts[2][1] = ImVec2(60.f, 0.1f);
-      mPts[2][2] = ImVec2(90.f, 0.82f);
-      mPts[2][3] = ImVec2(150.f, 0.24f);
-      mPts[2][4] = ImVec2(200.f, 0.34f);
-      mPts[2][5] = ImVec2(250.f, 0.12f);
-      mPointCount[2] = 6;
-      mbVisible[0] = mbVisible[1] = mbVisible[2] = true;
-      mMax = ImVec2(1.f, 1.f);
-      mMin = ImVec2(0.f, 0.f);
-   }
-   size_t GetCurveCount()
-   {
-      return 3;
-   }
-
-   bool IsVisible(size_t curveIndex)
-   {
-      return mbVisible[curveIndex];
-   }
-   size_t GetPointCount(size_t curveIndex)
-   {
-      return mPointCount[curveIndex];
-   }
-
-   uint32_t GetCurveColor(size_t curveIndex)
-   {
-      uint32_t cols[] = { 0xFF0000FF, 0xFF00FF00, 0xFFFF0000 };
-      return cols[curveIndex];
-   }
-   ImVec2* GetPoints(size_t curveIndex)
-   {
-      return mPts[curveIndex];
-   }
-   virtual ImCurveEdit::CurveType GetCurveType(size_t curveIndex) const { return ImCurveEdit::CurveSmooth; }
-   virtual int EditPoint(size_t curveIndex, int pointIndex, ImVec2 value)
-   {
-      mPts[curveIndex][pointIndex] = ImVec2(value.x, value.y);
-      SortValues(curveIndex);
-      for (size_t i = 0; i < GetPointCount(curveIndex); i++)
-      {
-         if (mPts[curveIndex][i].x == value.x)
-            return (int)i;
-      }
-      return pointIndex;
-   }
-   virtual void AddPoint(size_t curveIndex, ImVec2 value)
-   {
-      if (mPointCount[curveIndex] >= 8)
-         return;
-      mPts[curveIndex][mPointCount[curveIndex]++] = value;
-      SortValues(curveIndex);
-   }
-   virtual ImVec2& GetMax() { return mMax; }
-   virtual ImVec2& GetMin() { return mMin; }
-   virtual unsigned int GetBackgroundColor() { return 0; }
-   ImVec2 mPts[3][8];
-   size_t mPointCount[3];
-   bool mbVisible[3];
-   ImVec2 mMin;
-   ImVec2 mMax;
-private:
-   void SortValues(size_t curveIndex)
-   {
-      auto b = std::begin(mPts[curveIndex]);
-      auto e = std::begin(mPts[curveIndex]) + GetPointCount(curveIndex);
-      std::sort(b, e, [](ImVec2 a, ImVec2 b) { return a.x < b.x; });
-
-   }
-};
 
 struct MySequence : public ImSequencer::SequenceInterface
 {
@@ -153,7 +63,7 @@ struct MySequence : public ImSequencer::SequenceInterface
       bool mExpanded;
    };
    std::vector<MySequenceItem> myItems;
-   RampEdit rampEdit;
+   //RampEdit rampEdit;
 
    virtual void DoubleClick(int index) {
       if (myItems[index].mExpanded)
@@ -170,16 +80,16 @@ struct MySequence : public ImSequencer::SequenceInterface
    {
       static const char* labels[] = { "Translation", "Rotation" , "Scale" };
 
-      rampEdit.mMax = ImVec2(float(mFrameMax), 1.f);
-      rampEdit.mMin = ImVec2(float(mFrameMin), 0.f);
+      //rampEdit.mMax = ImVec2(float(mFrameMax), 1.f);
+      //rampEdit.mMin = ImVec2(float(mFrameMin), 0.f);
       draw_list->PushClipRect(legendClippingRect.Min, legendClippingRect.Max, true);
       for (int i = 0; i < 3; i++)
       {
          ImVec2 pta(legendRect.Min.x + 30, legendRect.Min.y + i * 14.f);
          ImVec2 ptb(legendRect.Max.x, legendRect.Min.y + (i + 1) * 14.f);
-         draw_list->AddText(pta, rampEdit.mbVisible[i] ? 0xFFFFFFFF : 0x80FFFFFF, labels[i]);
-         if (ImRect(pta, ptb).Contains(ImGui::GetMousePos()) && ImGui::IsMouseClicked(0))
-            rampEdit.mbVisible[i] = !rampEdit.mbVisible[i];
+         //draw_list->AddText(pta, rampEdit.mbVisible[i] ? 0xFFFFFFFF : 0x80FFFFFF, labels[i]);
+         //if (ImRect(pta, ptb).Contains(ImGui::GetMousePos()) && ImGui::IsMouseClicked(0))
+            //rampEdit.mbVisible[i] = !rampEdit.mbVisible[i];
       }
       draw_list->PopClipRect();
 
@@ -189,12 +99,12 @@ struct MySequence : public ImSequencer::SequenceInterface
 
    virtual void CustomDrawCompact(int index, ImDrawList* draw_list, const ImRect& rc, const ImRect& clippingRect)
    {
-      rampEdit.mMax = ImVec2(float(mFrameMax), 1.f);
-      rampEdit.mMin = ImVec2(float(mFrameMin), 0.f);
+      //rampEdit.mMax = ImVec2(float(mFrameMax), 1.f);
+      //rampEdit.mMin = ImVec2(float(mFrameMin), 0.f);
       draw_list->PushClipRect(clippingRect.Min, clippingRect.Max, true);
       for (int i = 0; i < 3; i++)
       {
-         for (int j = 0; j < rampEdit.mPointCount[i]; j++)
+         /*for (int j = 0; j < rampEdit.mPointCount[i]; j++)
          {
             float p = rampEdit.mPts[i][j].x;
             if (p < myItems[index].mFrameStart || p > myItems[index].mFrameEnd)
@@ -202,7 +112,7 @@ struct MySequence : public ImSequencer::SequenceInterface
             float r = (p - mFrameMin) / float(mFrameMax - mFrameMin);
             float x = ImLerp(rc.Min.x, rc.Max.x, r);
             draw_list->AddLine(ImVec2(x, rc.Min.y + 6), ImVec2(x, rc.Max.y - 4), 0xAA000000, 4.f);
-         }
+         }*/
       }
       draw_list->PopClipRect();
    }
@@ -233,7 +143,7 @@ void GameScene::init()
    	mySequence.mFrameMin = -100;
    	mySequence.mFrameMax = 1000;
    	mySequence.myItems.push_back(MySequence::MySequenceItem{ 0, 10, 30, false });
-   	mySequence.myItems.push_back(MySequence::MySequenceItem{ 1, 20, 30, true });
+   	mySequence.myItems.push_back(MySequence::MySequenceItem{ 1, 20, 30, false });
    	mySequence.myItems.push_back(MySequence::MySequenceItem{ 3, 12, 60, false });
    	mySequence.myItems.push_back(MySequence::MySequenceItem{ 2, 61, 90, false });
    	mySequence.myItems.push_back(MySequence::MySequenceItem{ 4, 90, 99, false });
