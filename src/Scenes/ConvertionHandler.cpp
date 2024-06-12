2e11355afb691c3f68b38b8a5c8569f579fb36b5
Gustavo Domingues <61640594+bigasdev@users.noreply.github.com>
2024-06-12 11:42:02 -0300

    Better quality + transparent fully working

--- src/Scenes/ConvertionHandler.cpp
+++ src/Scenes/ConvertionHandler.cpp
@@ -37,9 +37,8 @@ void Convertion::convert(std::vector<GifFrame> frames, std::string output, std::
         batchFile << "ren \"" << frames[i].m_file_path.substr(frames[i].m_file_path.find_last_of("\\") + 1) << "\" \"image" << i + 1 << extension << std::endl;
     }
     //batchFile << "ffmpeg -i image%%d.jpeg -vf \"pad=180:100:(ow-iw)/2:(oh-ih)/2\" -t 5 output.gif" << std::endl;
-    //generate pallete
-    batchFile << "ffmpeg -f concat -i files.txt -vf palettegen=reserve_transparent=1 palette.png" << std::endl;
-    batchFile << "ffmpeg -f concat -safe 0 -i files.txt -i palette.png -filter_complex \"[0:v]pad=388:377:(ow-iw)/2:(oh-ih)/2,paletteuse\" -t 15 " << output << std::endl;
+    batchFile << "ffmpeg -f concat -i files.txt -vf palettegen=max_colors=256:reserve_transparent=1 palette.png" << std::endl;
+    batchFile << "ffmpeg -f concat -safe 0 -i files.txt -i palette.png -filter_complex \"[0:v]pad=388:377:(ow-iw)/2:(oh-ih)/2,split[s0][s1];[s0]palettegen=max_colors=256:reserve_transparent=1,paletteuse=dither=bayer[b];[s1][b]paletteuse=dither=bayer\" -t 15 " << output << std::endl;
     for (size_t i = 0; i < frames.size(); ++i) {
         batchFile << "del image" << i + 1 << extension << std::endl;
         batchFile << "del palette.png" << std::endl;
