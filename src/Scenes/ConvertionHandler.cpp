3f272b6e3f3989410df42f7f5602c4bd1290134d
Gustavo Domingues <61640594+bigasdev@users.noreply.github.com>
2024-06-17 11:48:49 -0300

    Added fps to the gif so now the duration working correctly

--- src/Scenes/ConvertionHandler.cpp
+++ src/Scenes/ConvertionHandler.cpp
@@ -28,6 +28,10 @@ void Convertion::convert(std::vector<GifFrame> frames, std::string output, GifSe
 
 			int sum = frames[frame].frame_end - frames[frame].frame_start;
 
+            if(i == frames.size()){
+                sum = 1;
+            }
+
             int frame_img = Math::clamp(1, i+1, frames.size());
 
 			outfile << "file image" << frame_img << extension << std::endl;
@@ -50,7 +54,7 @@ void Convertion::convert(std::vector<GifFrame> frames, std::string output, GifSe
         batchFile << "ffmpeg -f concat -i files.txt -vf palettegen=max_colors=256:reserve_transparent=1 palette.png" << std::endl;
         batchFile << "ffmpeg -f concat -safe 0 -i files.txt -i palette.png -filter_complex \"[0:v]pad=388:377:(ow-iw)/2:(oh-ih)/2,split[s0][s1];[s0]palettegen=max_colors=256:reserve_transparent=1,paletteuse=dither=bayer[b];[s1][b]paletteuse=dither=bayer\" -t 15 "<< loop_option << output << std::endl;
     }else{
-        batchFile << "ffmpeg -f concat -safe 0 -i files.txt -vf \"pad="<<settings.m_width<<":"<<settings.m_height<<":(ow-iw)/2:(oh-ih)/2\" -t 15 " << loop_option << output << std::endl;
+        batchFile << "ffmpeg -f concat -safe 0 -i files.txt -vf \"fps=10,pad="<<settings.m_width<<":"<<settings.m_height<<":(ow-iw)/2:(oh-ih)/2\" " << loop_option << output << std::endl;
     }
     for (size_t i = 0; i < frames.size(); ++i) {
         batchFile << "del image" << i + 1 << extension << std::endl;
