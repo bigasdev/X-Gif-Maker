97e791e98ede7b5c57499950081a7c400da85d43
Gustavo Domingues <61640594+bigasdev@users.noreply.github.com>
2024-06-17 10:51:27 -0300

    image gif settings!

--- convert_images.bat
+++ convert_images.bat
@@ -1,10 +1,15 @@
 @echo off
 copy "C:\Users\Escritorio2\Downloads\ManipuladorSacaria.png" .
 ren "ManipuladorSacaria.png" "image1.png
-copy "C:\Users\Escritorio2\Downloads\ManipuladorMovel.png" .
-ren "ManipuladorMovel.png" "image2.png
-ffmpeg -f concat -safe 0 -i files.txt -vf "pad=696:564:(ow-iw)/2:(oh-ih)/2" -t 15 C:\Users\Escritorio2\Downloads\heyhe.gif
+copy "C:\Users\Escritorio2\Downloads\ManipuladorBaseMovel2.png" .
+ren "ManipuladorBaseMovel2.png" "image2.png
+copy "C:\Users\Escritorio2\Downloads\ManipuladorBaseMovel.png" .
+ren "ManipuladorBaseMovel.png" "image3.png
+ffmpeg -f concat -safe 0 -i files.txt -vf "pad=696:564:(ow-iw)/2:(oh-ih)/2" -t 15 -loop 1C:\Users\Escritorio2\Downloads\heyo.gif
 del image1.png
 del palette.png
 del image2.png
 del palette.png
+del image3.png
+del palette.png
+explorer C:\Users\Escritorio2\Downloads\heyo.gif
--- src/Scenes/ConvertionHandler.cpp
+++ src/Scenes/ConvertionHandler.cpp
@@ -45,15 +45,20 @@ void Convertion::convert(std::vector<GifFrame> frames, std::string output, GifSe
         batchFile << "ren \"" << frames[i].m_file_path.substr(frames[i].m_file_path.find_last_of("\\") + 1) << "\" \"image" << i + 1 << extension << std::endl;
     }
     //batchFile << "ffmpeg -i image%%d.jpeg -vf \"pad=180:100:(ow-iw)/2:(oh-ih)/2\" -t 5 output.gif" << std::endl;
+    std::string loop_option = settings.m_is_looping ? "-loop 0 " : "-loop 1 ";
     if(settings.m_is_transparent){
         batchFile << "ffmpeg -f concat -i files.txt -vf palettegen=max_colors=256:reserve_transparent=1 palette.png" << std::endl;
-        batchFile << "ffmpeg -f concat -safe 0 -i files.txt -i palette.png -filter_complex \"[0:v]pad=388:377:(ow-iw)/2:(oh-ih)/2,split[s0][s1];[s0]palettegen=max_colors=256:reserve_transparent=1,paletteuse=dither=bayer[b];[s1][b]paletteuse=dither=bayer\" -t 15 " << output << std::endl;
+        batchFile << "ffmpeg -f concat -safe 0 -i files.txt -i palette.png -filter_complex \"[0:v]pad=388:377:(ow-iw)/2:(oh-ih)/2,split[s0][s1];[s0]palettegen=max_colors=256:reserve_transparent=1,paletteuse=dither=bayer[b];[s1][b]paletteuse=dither=bayer\" -t 15 "<< loop_option << output << std::endl;
     }else{
-        batchFile << "ffmpeg -f concat -safe 0 -i files.txt -vf \"pad="<<settings.m_width<<":"<<settings.m_height<<":(ow-iw)/2:(oh-ih)/2\" -t 15 " << output << std::endl;
+        batchFile << "ffmpeg -f concat -safe 0 -i files.txt -vf \"pad="<<settings.m_width<<":"<<settings.m_height<<":(ow-iw)/2:(oh-ih)/2\" -t 15 " << loop_option << output << std::endl;
     }
     for (size_t i = 0; i < frames.size(); ++i) {
         batchFile << "del image" << i + 1 << extension << std::endl;
         batchFile << "del palette.png" << std::endl;
+    }
+
+    if(settings.m_is_open_folder){
+        batchFile << "explorer " << output << std::endl;
     }
 
     batchFile.close();
