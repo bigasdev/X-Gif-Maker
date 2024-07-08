600dc65d5939a54d1fecf5aade8d2f8461e1980d
Gustavo Domingues <61640594+bigasdev@users.noreply.github.com>
2024-07-08 10:56:03 -0300

    Added warning

--- src/Utils/FDebug.hpp
+++ src/Utils/FDebug.hpp
@@ -3,25 +3,26 @@
 #ifndef DEBUG_HPP
 #define DEBUG_HPP
 
-#include <iostream>
 #include <fstream>
 #include <iomanip>
-#include <vector>
-#include <string>
+#include <iostream>
 #include <sstream>
+#include <string>
+#include <vector>
 
 namespace F_Debug {
-    extern std::vector<std::string> logs;
-    
-    void log(std::string message);
-        
-    void log_group(std::string type, std::string value);
-    void error(std::string message);
+extern std::vector<std::string> logs;
 
-    void write_to_file(std::string filename);
-    void signal_handler(int signal);
-    void terminate_handler();
-    void setup_crash_handlers();
-}
+void log(std::string message);
+
+void log_group(std::string type, std::string value);
+void error(std::string message);
+void warning(std::string message);
+
+void write_to_file(std::string filename);
+void signal_handler(int signal);
+void terminate_handler();
+void setup_crash_handlers();
+} // namespace F_Debug
 
 #endif
